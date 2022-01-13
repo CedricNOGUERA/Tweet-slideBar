c4344620410cc06b7b3699dab04c6411a297232b
CedricNOGUERA <cedric.noguera@lwane.com>
2022-01-12 15:48:15 -1000

    slide bar without menu

---
+++ src/SidebarLink.js
@@ -0,0 +1,12 @@
+import { Icon } from "@material-ui/core";
+import "./sidebarLink.css";
+
+function SidebarLink({ text, Icon }) {
+	return (
+		<div className="link">
+			<Icon />
+			<h2>{text}</h2>
+		</div>
+	);
+}
+export default SidebarLink;
